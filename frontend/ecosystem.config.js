ad1ac154d049db3c3269ff17e0c4749be06e7fb2
kapitan4ever <stklmn@yandex.ru>
2023-05-11 21:01:57 +0300

    conf front

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,23 +1,11 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env.deploy") });
+require("dotenv").config({ path: ".env.deploy" });
 
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REPO,
-  DEPLOY_REF = "origin/master",
-} = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } =
+  process.env;
 
 module.exports = {
-  apps: [
-    {
-      name: "frontend",
-      script: "./build/index.html",
-    },
-  ],
+  apps: [],
 
-  // Настройка деплоя
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -25,7 +13,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": `cd ${DEPLOY_PATH}source/frontend && npm i && npm run build`,
+      "post-deploy": "scp -Сr ./build/* kapitan4ever@158.160.60.236:/home/kapitan4ever/web-plus-pm2-deploy/frontend",
     },
   },
 };
