d886345a1b10b30e8345aa3a046d68a760794526
kapitan4ever <stklmn@yandex.ru>
2023-05-11 21:10:58 +0300

    post-deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "post-deploy": "scp -Сr ./build/* kapitan4ever@158.160.60.236:/home/kapitan4ever/web-plus-pm2-deploy/frontend",
+      "post-deploy": "cd frontend && npm ci && npm run build",
     },
   },
 };
